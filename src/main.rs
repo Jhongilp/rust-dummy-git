35c3b42e8e7f4717d7f443bff5d2c197eb875601
Jonathan Gil <jhongilp@gmail.com>
2020-06-29 14:19:39 -0500

    add new error for git2 crate

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,61 @@
 use std::io;
 use std::fmt;
 use std::io::{Read,Write};
+use git2::Repository;
 
 fn main() -> Result<(), Error> {
-    crossterm::terminal::enable_raw_mode()?;
-
-    let mut stdout = io::stdout();
-    stdout.write_all(b"hello world\n").unwrap();
-
-    let mut stdin = io::stdin().bytes();
-    loop {
-        write!(stdout, "Type something > ").unwrap();
-        stdout.flush()?;
-        
-        let byte = match stdin.next() {
-            Some(b) => b?,
-            None => break,
-        };
-        
-        let c = char::from(byte);
-
-        if c == 'q' {
-            break;
-        }
-
-        write!(stdout, "You typed '{}'\n", c)?;
-        stdout.flush()?;
-    }
-
-    crossterm::terminal::disable_raw_mode().unwrap();
+    let repo = Repository::open_from_env()?;
 
     Ok(())
 }
+
+
+// fn main() -> Result<(), Error> {
+//     crossterm::terminal::enable_raw_mode()?;
+
+//     let mut stdout = io::stdout();
+//     stdout.write_all(b"hello world\n").unwrap();
+
+//     let mut stdin = io::stdin().bytes();
+//     loop {
+//         write!(stdout, "Type something > ").unwrap();
+//         stdout.flush()?;
+        
+//         let byte = match stdin.next() {
+//             Some(b) => b?,
+//             None => break,
+//         };
+        
+//         let c = char::from(byte);
+
+//         if c == 'q' {
+//             break;
+//         }
+
+//         write!(stdout, "You typed '{}'\n", c)?;
+//         stdout.flush()?;
+//     }
+
+//     crossterm::terminal::disable_raw_mode().unwrap();
+
+//     Ok(())
+// }
+
+// #[derive(Debug, thiserror::Error)]
+// enum Error {
+//     #[error(transparent)]
+//     CrosstermError(#[from] crossterm::ErrorKind),
+//     #[error(transparent)]
+//     IoError(#[from] io::Error),
+//     #[error(transparent)]
+//     GitError(#[from] git2::Error),
+// }
 
 #[derive(Debug)]
 enum Error {
     CrosstermError(crossterm::ErrorKind),
     IoError(io::Error),
+    GitError(git2::Error)
 }
 
 impl From<crossterm::ErrorKind> for Error {
@@ -51,11 +70,18 @@ impl From<io::Error> for Error {
     }
 }
 
+impl From<git2::Error> for Error {
+    fn from(error: git2::Error) -> Error {
+        Error::GitError(error)
+    }
+}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Error::CrosstermError(inner) => write!(f, "{}", inner),
             Error::IoError(inner) => write!(f, "{}", inner),
+            Error::GitError(inner) => write!(f, "{}", inner),
         }
     }
 }
@@ -65,6 +91,7 @@ impl std::error::Error for Error {
         match self {
             Error::CrosstermError(inner) => Some(inner),
             Error::IoError(inner) => Some(inner),
+            Error::GitError(inner) => Some(inner),
         }
     }
 }
